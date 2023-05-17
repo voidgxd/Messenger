5e33bd44c29fe443aab6870037cb16277cbc21c5
voidgxd <118388964+voidgxd@users.noreply.github.com>
2023-05-17 12:32:18 +0300

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Максим Мосалёв on 17.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
