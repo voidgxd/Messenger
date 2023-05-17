91f99bdc09951a56e194cf90850cbaea1019dca5
voidgxd <118388964+voidgxd@users.noreply.github.com>
2023-05-17 12:43:58 +0300

    Initial commit

--- Messenger/ViewController.swift
+++ Messenger/ViewController.swift
@@ -12,8 +12,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .blue
     }
 
 
 }
 
+import SwiftUI
+
+struct ViewControllerProvider: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+        
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        let viewController = ViewController()
+        
+        func makeUIViewController(context: Context) -> ViewController {
+            return viewController
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+            
+        }
+    }
+}
